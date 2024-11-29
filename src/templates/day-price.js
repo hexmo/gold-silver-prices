807639ab3e4ee13c9846ccc9b41c8668b746b8b7
Mohit Acharya <mohitacharya100@gmail.com>
2024-11-29 14:59:53 +0545

    fix: configure tailwind properly

--- src/templates/day-price.js
+++ src/templates/day-price.js
@@ -9,44 +9,42 @@ const DayPrice = ({ pageContext }) => {
 
   return (
     <div className="container mx-auto p-6">
-      <h1 className="text-2xl font-bold mb-64">Prices for {date}</h1>
+      <h1 className="text-6xl font-bold mb-64">Prices for {date}</h1>
       <div className="flex flex-row row-span-3">
+        {/* Fine Gold */}
+        <div className="mb-6">
+          <img
+            src={fineGoldImage}
+            alt="Fine Gold Bar"
+            className="mx-auto rounded shadow"
+          />
+          <h2 className="text-xl font-semibold text-center mt-4">Fine Gold</h2>
+          <p className="text-center">Price: NPR {fineGold}</p>
+        </div>
 
+        {/* Standard Gold */}
+        <div className="mb-6">
+          <img
+            src={standardGoldImage}
+            alt="Standard Gold Bar"
+            className="w-64 mx-auto rounded shadow"
+          />
+          <h2 className="text-xl font-semibold text-center mt-4">
+            Standard Gold
+          </h2>
+          <p className="text-center">Price: NPR {standardGold}</p>
+        </div>
 
-      {/* Fine Gold */}
-      <div className="mb-6">
-        <img
-          src={fineGoldImage}
-          alt="Fine Gold Bar"
-          className="mx-auto rounded shadow"
-        />
-        <h2 className="text-xl font-semibold text-center mt-4">Fine Gold</h2>
-        <p className="text-center">Price: NPR {fineGold}</p>
-      </div>
-
-      {/* Standard Gold */}
-      <div className="mb-6">
-        <img
-          src={standardGoldImage}
-          alt="Standard Gold Bar"
-          className="w-64 mx-auto rounded shadow"
-        />
-        <h2 className="text-xl font-semibold text-center mt-4">
-          Standard Gold
-        </h2>
-        <p className="text-center">Price: NPR {standardGold}</p>
-      </div>
-
-      {/* Silver */}
-      <div className="mb-6">
-        <img
-          src={silverImage}
-          alt="Silver Bar"
-          className="w-64 mx-auto rounded shadow"
-        />
-        <h2 className="text-xl font-semibold text-center mt-4">Silver</h2>
-        <p className="text-center">Price: NPR {silver}</p>
-      </div>
+        {/* Silver */}
+        <div className="mb-6">
+          <img
+            src={silverImage}
+            alt="Silver Bar"
+            className="w-64 mx-auto rounded shadow"
+          />
+          <h2 className="text-xl font-semibold text-center mt-4">Silver</h2>
+          <p className="text-center">Price: NPR {silver}</p>
+        </div>
       </div>
 
       {/* Navigation Buttons */}
